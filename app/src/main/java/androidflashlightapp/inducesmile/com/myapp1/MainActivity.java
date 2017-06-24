943ed39ead8618593b6d552f8c47b17eb162f3aa
dishamahajan <dishamahajan28@gmail.com>
2017-06-24 21:08:14 +0530

    String name changes Permission changes

--- app/src/main/java/androidflashlightapp/inducesmile/com/myapp1/MainActivity.java
+++ app/src/main/java/androidflashlightapp/inducesmile/com/myapp1/MainActivity.java
@@ -18,27 +18,16 @@ public class MainActivity extends Activity {
 
     private CameraManager objCameraManager;
     private String mCameraId;
-    private ImageView ivOnOFF;
-    private MediaPlayer objMediaPlayer;
-
-    /**
-     * for getting torch mode
-     */
+    private ImageView isOnOFF;
     private Boolean isTorchOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ivOnOFF = (ImageView) findViewById(R.id.ivOnOFF);
+        isOnOFF = (ImageView) findViewById(R.id.isOnOFF);
         isTorchOn = false;
 
-        /**
-         * Check if device contains flashlight
-         *
-         * if not then exit from screen
-         *
-         */
         Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
         if (!isFlashAvailable) {
@@ -47,35 +36,38 @@ public class MainActivity extends Activity {
             alert.setMessage(getString(R.string.msg_error));
             alert.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.lbl_ok), new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
-                    finish();
+            finish();
            }
             });
             alert.show();
             return;
         }
 
-        String[] perms = {"android.permission.FLASHLIGHT", "android.permission.CAMERA"};
-        int permsRequestCode = 200;
-        requestPermissions(perms,permsRequestCode);
+        permissionDialogBox();
+
         objCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             mCameraId = objCameraManager.getCameraIdList()[0];
         } catch (CameraAccessException e) {
             e.printStackTrace();
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        ivOnOFF.setOnClickListener(new View.OnClickListener() {
+        isOnOFF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    if (isTorchOn) {
-                        turnOffLight();
-                        isTorchOn = false;
-                    } else {
-                        turnOnLight();
-                        isTorchOn = true;
+                    if(canMakeSmores() && !hasPermission("android.permission.CAMERA")){
+                        permissionDialogBox();
+                    }else{
+                        if (isTorchOn) {
+                            turnOffLight();
+                            isTorchOn = false;
+                        } else {
+                            turnOnLight();
+                            isTorchOn = true;
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -84,16 +76,21 @@ public class MainActivity extends Activity {
         });
     }
 
+    private void permissionDialogBox() {
+        String[] perms = {"android.permission.FLASHLIGHT", "android.permission.CAMERA"};
+        int permsRequestCode = 200;
+        requestPermissions(perms,permsRequestCode);
+    }
+
     /**
      * Method for turning light ON
      */
     public void turnOnLight() {
-
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 objCameraManager.setTorchMode(mCameraId, true);
-                playOnOffSound();
-                ivOnOFF.setImageResource(R.drawable.on);
+                //playOnOffSound();
+                isOnOFF.setImageResource(R.drawable.on);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,30 +101,15 @@ public class MainActivity extends Activity {
      * Method for turning light OFF
      */
     public void turnOffLight() {
-
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 objCameraManager.setTorchMode(mCameraId, false);
-                playOnOffSound();
-                ivOnOFF.setImageResource(R.drawable.off);
-
+                //playOnOffSound();
+                isOnOFF.setImageResource(R.drawable.off);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void playOnOffSound() {
-        objMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.flash_sound);
-        objMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.release();
-            }
-        });
-        objMediaPlayer.start();
     }
 
     @Override
