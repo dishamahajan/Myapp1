ab1b7436d48aeb81f846c37256fa443a95ba73c2
dishamahajan <dishamahajan28@gmail.com>
2017-06-27 01:56:33 +0530

    Added support for all android versions

--- app/src/main/java/androidflashlightapp/inducesmile/com/myapp1/MainActivity.java
+++ app/src/main/java/androidflashlightapp/inducesmile/com/myapp1/MainActivity.java
@@ -1,30 +1,47 @@
 package androidflashlightapp.inducesmile.com.myapp1;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Size;
+import android.view.Surface;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toolbar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     private CameraManager objCameraManager;
     private String mCameraId;
     private ImageButton isOnOFF;
     private Boolean isTorchOn;
     private AdView mAdView;
+    private Camera camera;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession session;
+    private CaptureRequest.Builder builder;
+    Camera.Parameters parameters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,36 +58,61 @@ public class MainActivity extends Activity {
             alert.setMessage(getString(R.string.msg_error));
             alert.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.lbl_ok), new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
-            finish();
-           }
+                    finish();
+                }
             });
             alert.show();
             return;
         }
 
-        permissionDialogBox();
 
-        objCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            mCameraId = objCameraManager.getCameraIdList()[0];
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionDialogBox();
+            objCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try {
+                mCameraId = objCameraManager.getCameraIdList()[0];
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            permissionDialogBox();
+            objCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try {
+                CameraCharacteristics cameraCharacteristics = objCameraManager.getCameraCharacteristics("0");
+                boolean flashAvailable = cameraCharacteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                if (flashAvailable) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        objCameraManager.openCamera("0", new CameraDeviceStateCallback(), null);
+                    }
+                }
+            } catch (Exception e) {
+
+            }
+        } else {
+            getCamera();
         }
-
         isOnOFF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    if(!hasPermission("android.permission.CAMERA")){
+                    if (checkVersion() && !hasPermission("android.permission.CAMERA")) {
                         permissionDialogBox();
-                    }else{
+                    } else {
                         if (isTorchOn) {
-                            turnOffLight();
+                            if (camera != null) {
+                                turnOffFlash();
+                            } else {
+                                turnOffLight();
+                            }
                             isTorchOn = false;
                         } else {
-                            turnOnLight();
+                            if (camera != null) {
+                                turnOnFlash();
+                            } else {
+                                turnOnLight();
+                            }
                             isTorchOn = true;
                         }
                     }
@@ -87,10 +129,78 @@ public class MainActivity extends Activity {
         mAdView.loadAd(adRequest);
     }
 
+    class CameraDeviceStateCallback extends CameraDevice.StateCallback {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            cameraDevice = camera;
+            try {
+                builder = camera.createCaptureRequest(CameraDevice.TEMPLATE_MANUAL);
+                builder.set(CaptureRequest.CONTROL_AE_MODE, CameraMetadata.CONTROL_AF_MODE_AUTO);
+                builder.set(CaptureRequest.FLASH_MODE, CameraMetadata.FLASH_MODE_TORCH);
+                List<Surface> list = new ArrayList<Surface>();
+                SurfaceTexture mSurfaceTexture = new SurfaceTexture(1);
+                Size size = getSmallestSize(cameraDevice.getId());
+                mSurfaceTexture.setDefaultBufferSize(size.getWidth(), size.getHeight());
+                Surface mSurface = new Surface(mSurfaceTexture);
+                list.add(mSurface);
+                builder.addTarget(mSurface);
+                camera.createCaptureSession(list, new CameraCaptureSessionStateCallback(), null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+
+        }
+    }
+
+    class CameraCaptureSessionStateCallback extends CameraCaptureSession.StateCallback {
+
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            MainActivity.this.session = session;
+            try {
+                MainActivity.this.session.setRepeatingRequest(builder.build(), null, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+
+        }
+    }
+
+    private Size getSmallestSize(String cameraId) throws CameraAccessException {
+        Size[] outputSizes = objCameraManager.getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                .getOutputSizes(SurfaceTexture.class);
+        if (outputSizes == null || outputSizes.length == 0) {
+            throw new IllegalStateException(
+                    "Camera " + cameraId + "doesn't support any outputSize.");
+        }
+        Size chosen = outputSizes[0];
+        for (Size s : outputSizes) {
+            if (chosen.getWidth() >= s.getWidth() && chosen.getHeight() >= s.getHeight()) {
+                chosen = s;
+            }
+        }
+        return chosen;
+    }
+
     private void permissionDialogBox() {
         String[] perms = {"android.permission.FLASHLIGHT", "android.permission.CAMERA"};
         int permsRequestCode = 200;
-        requestPermissions(perms,permsRequestCode);
+        requestPermissions(perms, permsRequestCode);
     }
 
     public void turnOnLight() {
@@ -98,6 +208,14 @@ public class MainActivity extends Activity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 objCameraManager.setTorchMode(mCameraId, true);
                 isOnOFF.setImageResource(R.drawable.on);
+            } else {
+                try {
+                    builder.set(CaptureRequest.FLASH_MODE, CameraMetadata.FLASH_MODE_TORCH);
+                    session.setRepeatingRequest(builder.build(), null, null);
+                    isOnOFF.setImageResource(R.drawable.on);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -109,6 +227,14 @@ public class MainActivity extends Activity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 objCameraManager.setTorchMode(mCameraId, false);
                 isOnOFF.setImageResource(R.drawable.off);
+            } else {
+                try {
+                    builder.set(CaptureRequest.FLASH_MODE, CameraMetadata.FLASH_MODE_OFF);
+                    session.setRepeatingRequest(builder.build(), null, null);
+                    isOnOFF.setImageResource(R.drawable.off);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -120,6 +246,10 @@ public class MainActivity extends Activity {
         super.onStop();
         if (isTorchOn) {
             turnOffLight();
+        }
+        if (camera != null) {
+            camera.release();
+            camera = null;
         }
     }
 
@@ -134,14 +264,53 @@ public class MainActivity extends Activity {
     }
 
     private boolean hasPermission(String permission) {
-        if (canMakeSmores()) {
+        if (checkVersion()) {
             return (checkSelfPermission(permission)) == PackageManager.PERMISSION_GRANTED;
         }
         return true;
     }
 
-    private boolean canMakeSmores() {
-        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    private boolean checkVersion() {
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
     }
 
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                parameters = camera.getParameters();
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    private void turnOnFlash() {
+        if (!isTorchOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            isOnOFF.setImageResource(R.drawable.on);
+            isTorchOn = true;
+        }
+    }
+
+
+    private void turnOffFlash() {
+        if (isTorchOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            isOnOFF.setImageResource(R.drawable.off);
+            isTorchOn = false;
+        }
+    }
 }
